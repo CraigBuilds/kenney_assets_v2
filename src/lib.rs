655710b8cff4ca4828c7e0dbbf46d4a67827186a
Craig <craig.hickman@ukaea.uk>
2024-03-01 16:40:01 +0000

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,18 @@ use home;
 pub fn assets_path() -> PathBuf {
     let mut cargo_path = home::cargo_home().expect("Could not find cargo home");
     cargo_path.push("git");
+    cargo_path.push("checkouts");
     cargo_path.push("assets");
-    cargo_path
+    //find all the directories in the assets folder that start with "kenney_assets_v2"
+    let all_dirs = cargo_path.read_dir().expect(&format!("Could not read {:?}", cargo_path));
+    let mut matching_dirs = all_dirs.filter(|dir| {
+        let dir = dir.as_ref().expect("Could not read dir");
+        let name = dir.file_name();
+        let name = name.to_str().expect("Could not convert dir name to str");
+        name.starts_with("kenney_assets_v2")
+    });
+    //use the first matching directory
+    let matching_dir = matching_dirs.next().expect("Could not find matching directory");
+    let matching_dir = matching_dir.expect("Could not read matching directory");
+    matching_dir.path()
 }
